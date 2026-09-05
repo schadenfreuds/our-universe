e76589995939638c65e516cbbb7bef9fff6f7fd7
schadenfreuds <schadenfreuds@gmail.com>
2026-09-06 02:25:02 +0300

    chore: add 1-click baslat.bat launcher

---
+++ baslat.bat
@@ -0,0 +1,11 @@
+@echo off
+title Our Universe - Can & Ece
+echo ========================================================
+echo   Our Universe (Can & Ece) - Yerel Gelistirme Sunucusu
+echo ========================================================
+echo.
+echo Sunucu baslatiliyor... Tarayicida http://localhost:5173 acilacak.
+echo.
+start http://localhost:5173
+npm run dev
+pause
